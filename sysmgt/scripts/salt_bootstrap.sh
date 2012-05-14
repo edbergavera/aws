4c82dbfc1eeb4322f83078a2dc78d765ffdf7426
Eduardo Bergavera <edbergavera@gmail.com>
2012-05-14 18:20:24 +0800

    2nd commit

---
+++ sysmgt/scripts/salt_bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install saltstack
+add-apt-repository ppa:saltstack/salt -y
+apt-get update -y
+apt-get install salt-minion -y
+apt-get upgrade -y
+
+# Set salt master location and start minion
+cp /etc/salt/minion.template /etc/salt/minion
+sed -i -e 's/#master: salt/master: web.linuxd.org/' /etc/salt/minion
+salt-minion -d
